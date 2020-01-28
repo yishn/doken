d6392e44f26a9a647874e966c95cf51eb724bcfb
Yichuan Shen <shen.yichuan@gmail.com>
2020-01-29 00:32:04 +0100

    Copy & adapt code

---
+++ src/main.js
@@ -0,0 +1,86 @@
+exports.regexRule = function(type, regex, transform = match => match[0]) {
+  return [
+    type,
+    input => {
+      let match = regex.exec(input)
+      if (match == null) return null
+
+      return {
+        length: match[0].length,
+        value: transform(match)
+      }
+    }
+  ]
+}
+
+exports.createTokenizer = function({rules, shouldStop = token => false}) {
+  return function tokenize(input) {
+    let row = 0
+    let col = 0
+    let pos = 0
+    let restInput = input
+    let finished = false
+
+    return {
+      next() {
+        while (true) {
+          if (finished || restInput.length === 0) return {done: true}
+
+          let token = null
+
+          for (let [type, rule] of rules) {
+            let match = rule(restInput)
+            if (match == null) continue
+
+            token = {
+              type,
+              value: match.value,
+              row,
+              col,
+              pos,
+              length: match.length
+            }
+          }
+
+          if (token == null) {
+            token = {
+              type: null,
+              value: restInput[0],
+              row,
+              col,
+              pos,
+              length: 1
+            }
+          }
+
+          if (shouldStop(token)) {
+            finished = true
+          }
+
+          // Update source position
+
+          let newlineIndices = Array.from(restInput.slice(0, token.length))
+            .map((c, i) => (c === '\n' ? i : null))
+            .filter(x => x != null)
+
+          row += newlineIndices.length
+
+          if (newlineIndices.length > 0) {
+            col = token.length - newlineIndices.slice(-1)[0] - 1
+          } else {
+            col += token.length
+          }
+
+          pos += token.length
+          restInput = restInput.slice(token.length)
+
+          // Return token
+
+          if (token.type[0] !== '_') {
+            return {value: token, done: false}
+          }
+        }
+      }
+    }
+  }
+}
