28ddb59bb1361c0cfbb8f50cec8e9c4165600ced
Yichuan Shen <shen.yichuan@gmail.com>
2020-04-08 21:18:00 +0200

    Make token type typable

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,5 +1,5 @@
-export interface Token {
-  type: string | null
+export interface Token<T extends string> {
+  type: T | null
   value: any
   row: number
   col: number
@@ -7,8 +7,8 @@ export interface Token {
   length: number
 }
 
-export interface Rule {
-  type: string
+export interface Rule<T extends string> {
+  type: T
   lineBreaks?: boolean
   match(input: string, position: number): RuleMatch | null
 }
@@ -18,17 +18,17 @@ export interface RuleMatch {
   value?: any
 }
 
-export function regexRule(
-  type: string,
+export function regexRule<T extends string>(
+  type: T,
   regex: RegExp,
   options: {
     lineBreaks?: boolean
     value?: (match: RegExpExecArray) => any
     condition?: (match: RegExpExecArray) => boolean
   }
-): Rule
+): Rule<T>
 
-export function createTokenizer(options: {
-  rules: Rule[]
+export function createTokenizer<T extends string>(options: {
+  rules: Rule<T>[]
   strategy?: 'first' | 'longest'
-}): (input: string) => IterableIterator<Token>
+}): (input: string) => IterableIterator<Token<T>>
